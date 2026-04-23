3e98c34201a6d4fb36d1cc281903a3ae4a54fe45
Mario Codes <mariocodes1993@gmail.com>
2026-04-23 11:22:41 +0200

    duplicate Razor Pages projects

---
+++ csharp-razor-pages-multiproject/razor-pages/Pages/Courses/Details.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPages.Data;
+using RazorPages.Models;
+
+namespace razor_pages.Pages.Courses
+{
+    public class DetailsModel(SchoolContext context) : PageModel
+    {
+
+        public Course Course { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await context.Courses.FirstOrDefaultAsync(m => m.CourseID == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Course = course;
+            }
+            return Page();
+        }
+    }
+}
---
+++ csharp-razor-pages-multiproject/razor-pages/Pages/Students/Edit.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPages.Data;
+using RazorPages.Models;
+
+namespace RazorPages.Pages.Students
+{
+    public class EditModel(SchoolContext context) : PageModel
+    {
+
+        [BindProperty]
+        public Student Student { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student =  await context.Students.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            var studentToUpdate = await context.Students.FindAsync(id);
+
+            if(studentToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            if(await TryUpdateModelAsync<Student>(
+                studentToUpdate,
+                "student",
+                s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
+            {
+                await context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+            return Page();
+        }
+    }
+}
